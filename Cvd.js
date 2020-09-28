fe89dd9f921d69b1f5ac528e2cfc137e1b39b94c
Abhi Projects <cool.abhishekerketta@gmail.com>
2020-09-28 10:55:35 +0530

    changes

--- Cvd.js
+++ Cvd.js
@@ -105,18 +105,264 @@ for(let key in samsung){
 
 */
 
-
-
-
-
-
-
+/*
 var b={};
 
 console.log(Object.prototype);
 console.log(Array.prototype);
 console.log(b);
 console.log(null);
+console.log("Hello Abhishek");
+*/
+
+
+/*
+
+//Super class
+   var Mobile=function(){
+      
+   }
+
+   //Prototype of Mobile
+   Mobile.prototype.getModel=function(){
+       return this.model;
+   };
+
+
+   //Sub class
+   var Samsung=function(model,price){
+       this.model=model;
+       this.price=price;
+   }
+
+   //Sub class
+
+   var Lenovo=function(model){
+    this.model=model;
+    }
+    var Redmi=function(){
+
+    }
+
+    var Sony=function(){
+
+    };
+
+    //Creating Inheritance Function
+
+    function Inherit(child,parent){
+
+    child.prototype=Object.create(parent.prototype);
+
+    child.prototype.constructor=child;
+
+    }
+
+    //Inheritance
+    Inherit(Samsung,Mobile);
+    Inherit(Lenovo,Mobile);
+    Inherit(Redmi,Mobile);
+    Inherit(Sony,Mobile)
+
+
+    //Samsung Protoype function
+    Samsung.prototype.getPrice=function(){
+        return this.price;
+    }
+
+    Redmi.prototype.getram=function(){
+        return "3GB";
+    }
+    
+    var Galaxy=new Samsung("Galaxy",6000);
+    console.log(Galaxy);
+    console.log(Galaxy.getModel())
+
+    var Core=new Lenovo("Prime");
+
+    var Note=new Redmi()
+    console.log(Core);
+    console.log(Galaxy.getPrice());
+    console.log(Note.getram());
+
+*/
+
+
+/*
+var mobile=function(model){
+    this.model=model;
+}
+
+mobile.prototype.getModel=function(){
+    return this.mdoel;
+}
+
+
+
+var Samsung=function(price){
+    mobile.call(this);
+    this.price=price;
+}
+
+//Inheritance code
+
+Samsung.prototype=Object.create(mobile.prototype);
+Samsung.prototype.constructor=Samsung;
+
+//Creating Object from Samsung
+
+var Galaxy=new Samsung("Galaxy");
+console.log(Galaxy.getModel());
+
+*/
+
+
+/*
+//Super Class
+var Mobile=function(){
+
+}
+
+//Prototype Member
+Mobile.prototype.show=function(){
+    return "Super Class Method";
+}
+
+//Sub class
+var samsung=function(){
+
+}
+
+//function for Inheriting Sub class form Mobile
+
+function Inherit(child,parent){
+    child.prototype=Object.create(parent.prototype); //Inheriting the values from prototype
+    child.prototype.constructor = child; //reseting constuctor
+}
+
+Inherit(samsung,Mobile);
+
+samsung.prototype.show=function(){
+    return "Sub Class method";
+}
+
+
+//Creating Object of sub class Samsung
+
+var sam=new samsung();
+
+console.log(sam.show());
+
+*/
+
+
+
+//  Multi-Level Inheritance
+
+/*
+//Super class
+
+var Mobile=function(){    //
+    this.a=10;
+}
+
+
+//prototype of Mobile
+Mobile.prototype.z=30;
+
+var samsung =function(){
+    this.b=20;
+}
+
+var Galaxy= function(){
+    samsung.call(this);
+    this.c=100;
+}
+
+//Prototype  Inheritance
+    samsung.prototype=Object.create(Mobile.prototype);
+    samsung.prototype.constuctor=samsung;
+
+    Galaxy.prototype=Object.create(samsung.prototype);
+    Galaxy.prototype.constructor=Galaxy;
+
+
+    var m=new Mobile();   //Object Instatiate Mobile
+    var s=new samsung();  //Object Instatiate samsung
+    var g=new Galaxy();  //Object Instatiate Galaxy
+
+    console.log(g.a);
+
+*/
+
+/*      Composition or Mixin   */
+/*
+var eating ={
+    eat:function(){
+        return "I can Eat ";
+    }
+};
+
+var walking ={
+    walk:function(){
+        return "I can Walk ";
+    }
+};
+
+var talking ={
+    talk:function(){
+        return "I can Talk";
+    }
+};
+
+var Rahul= Object.assign({},eating,walking,talking)
+console.log(Rahul.eat());
+
+var Robot=Object.assign({},walking,talking);
+console.log(Robot.walk());
+*/
+
+//Class Declaration
+
+/*
+var Mobile=class{
+    constructor(model,color){
+        this.model=model,
+        this.color=color
+    }
+    show(){
+        return this.model+" Price Rs-"+this.color;
+    }
+    
+};
+
+var nokia=new Mobile("Galaxy","Red Cherry");
+
+console.log(nokia.show(500));
+*/
+
+/*
+class Father{
+    constructor(money){
+        this.fmoney=money;
+    }
+    showFmoney(){
+        return this.fmoney;
+    }
+}
+
+class Son extends Father{
+    showSmoney(){
+        return "Son's Money";
+    }
+}
+
+var s=new Son(1000);
+
+console.log(s.showFmoney());
+console.log(s.showSmoney());
+
+*/
 
 
 
